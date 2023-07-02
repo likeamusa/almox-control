07b01a0d71763116606b70ab53dc4ed651c864a2
Ítalo Kairon <italocentrismo@example.com>
2023-07-02 14:30:30 -0300

    feat: change to postgresql

--- db.js
+++ db.js
@@ -2,13 +2,17 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const database = new Sequelize({
-    dialect: 'mssql',
+    dialect: 'postgres',
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME,
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     logging: false,
+    ssl: true,
+    dialectOptions: {
+        ssl: {}
+    }
 });
 
 database.sync({
